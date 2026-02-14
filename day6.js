3caee6d296bc36cc1225e8a8233d713628cd83f7
potdaraayush <potdaraayush2023@gmail.com>
2026-02-15 00:00:13 +0530

    day 6

--- day6.js
+++ day6.js
@@ -1,21 +1,32 @@
-//1.
-function getData() {
-    const np = new Promise((res, rej) => {
-    let x = Math.random();
-    if(x > 0.5) {
-        setTimeout(() => {
-            res("data received.")
-        }, 1000);
-    } else {
-        rej("error occurred");
-    }
-    })
-    return np;
-}
-getData()
-.then(result => {
-    console.log(result);
-})
-.catch(err => {
-    console.log(err);
+// 1.
+// function getData() {
+//     const np = new Promise((res, rej) => {
+//     let x = Math.random();
+//     if(x > 0.5) {
+//         setTimeout(() => {
+//             res("data received.")
+//         }, 1000);
+//     } else {
+//         rej("error occurred");
+//     }
+//     })
+//     return np;
+// }
+// getData()
+// .then(result => {
+//     console.log(result);
+// })
+// .catch(err => {
+//     console.log(err);
+// });
+
+//2.
+setTimeout(() => {
+    console.log("timeout");
+}, 0);
+Promise.resolve().then(() => {
+    console.log("promise");
 });
+console.log("sync");
+
+// output : sync, promise, timeout
