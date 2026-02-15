4fd60b24f4dbe1bf076743383173dd1a60dc8fec
potdaraayush <potdaraayush2023@gmail.com>
2026-02-15 23:44:13 +0530

    its day 7

---
+++ day7.js
@@ -0,0 +1,25 @@
+//1. delayed multiplier
+//3. add error handling
+function multiplyAsync(num) {
+    return new Promise((res, rej) => {
+        if(num < 0) rej("-ve numbers are not allowed.");
+        else {setTimeout(() => {
+            res(num*2);
+        }, 1000);}
+    })
+}
+
+multiplyAsync(-10)
+.then(result => {
+    return multiplyAsync(result);
+})
+.then(result => {
+    return multiplyAsync(result);
+})
+//2. chain multiplyAsync thrice and log the final result
+.then(result => {
+    console.log(result);
+})
+.catch(error => {
+    console.log(error);
+})
