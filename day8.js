28ff1fb9d6ccd0d6b77f15e3f70dc3507b693a67
potdaraayush <potdaraayush2023@gmail.com>
2026-02-16 23:14:57 +0530

    day 8 q1, q2 and q3

---
+++ day8.js
@@ -0,0 +1,45 @@
+function asyncMultiplier(num) {
+    return new Promise((resolve, reject) => {
+        if(num > 0) {
+            setTimeout(()=>{
+                resolve(num*2);
+            }, 1000);
+        } else {
+            reject("-ve numbers not allowed babu");
+        }
+    })
+}
+
+async function run() {
+    try {
+        // let res = await asyncMultiplier(5);
+        // console.log(res);
+        // let res2 = await asyncMultiplier(-3);
+        // console.log(res2);
+
+        //q3
+        // const a = await asyncMultiplier(5);
+        // const b = await asyncMultiplier(3);
+        // console.log(a, b);
+
+        const [a, b] = await Promise.all([
+            asyncMultiplier(5), 
+            asyncMultiplier(3)
+        ]);
+
+        console.log(a, b);
+        
+        
+    } catch(error) {
+        console.error(error);
+    }
+    
+}
+
+run();
+
+
+// const [a, b] = await Promise.all([
+//     multiplyAsync(5),
+//     multiplyAsync(3)
+// ])
