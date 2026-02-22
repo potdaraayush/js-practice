28a675aa731c06aa11ff587b8c2555c5ce0cd437
potdaraayush <potdaraayush2023@gmail.com>
2026-02-22 23:51:21 +0530

    q2 pushing cause github aint green enough

--- day14.js
+++ day14.js
@@ -1,5 +1,16 @@
+// q1
 const name = "alice";
 const age = 24;
 
 console.log("my name is "+name+" and im "+age+" years old.");
 console.log(`my name is ${name} and im ${age} years old.`);
+
+// q2
+function greet(name = "guest") {
+    console.log(`hello, ${name}`);
+}
+
+greet("yush");
+greet();
+
+// q3
