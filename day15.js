1d41d9368fadc76ac21d7de46aac518805563ad8
potdaraayush <potdaraayush2023@gmail.com>
2026-02-23 23:48:30 +0530

    day 15 completed

--- day15.js
+++ day15.js
@@ -22,3 +22,33 @@ const sum = numbers.reduce((acc, curr) => {
     return acc + curr;
 }, 0);
 console.log(sum);
+
+// q4.
+const arr = [1, 2, [[3, 4], [5, 6]]];
+console.log(arr.flat(2));
+
+const words = ["hello js", "js is fun"];
+const newWords = words.flatMap((x) => x.split(" "));
+console.log(newWords);
+
+
+// q5.
+const orders = [
+    {id: 1, "amount": 100},
+    {id: 2, "amount": 200},
+    {id: 3, "amount": 150}
+]
+
+// 5a.
+const revenue = orders.reduce((acc, curr) => {
+    return acc+curr.amount;
+}, 0);
+console.log(revenue);
+
+// 5b.
+const idArray = orders.filter(order => order.amount > 120).map(order => order.id);
+console.log(idArray);
+
+// 5c.
+const ans = orders.some(order => order.amount < 100);
+console.log(ans);
